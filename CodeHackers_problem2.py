3c7532681da7e1ed6e059d03bbb072ae8c4229cf
HimeshKrishna <himeshkrishna2006@gmail.com>
2025-04-02 14:25:49 +0530

    Add files via upload

--- CodeHackers_problem2.py
+++ CodeHackers_problem2.py
@@ -1,12 +1,27 @@
-#himesh krishna
-import math
-I=1
-X=10
-C=100
+#himesh krishna 
+z={'I':1,'V':5,'X':10,'C':100,'M':500,'D':1000}
+print("Enter from within the given list ",z)
 A=input("Enter the first numeral: ")
 B=input("Enter the second numeral: ")
-D=int(A)
-E=int(B)
+D=0
+E=0
+for i in range(len(A)):
+        for j in z:
+            if(A[i]==j):
+                    D=D+z[j]
+for i in range(len(B)):
+        for j in z:
+            if(B[i]==j):
+                    E=E+z[j]
+print("Numeric Equivalent of first numeral = ",D)
+print("Numeric Equivalent of second numeral = ",E)
 Z=D+E
-F=(Z)
-print(F)
+print("Sum of both numerals = ",Z)
+F=str(Z);
+R=''
+for i in F:
+        for j in z:
+                if i==z[j]:
+                        for i in range(int(i)):
+                                R=R+j
+print(R)
